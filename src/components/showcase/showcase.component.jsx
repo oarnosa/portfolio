a2a91eac64114a28ff0698370af71cbdb0b724af
Orlando Arnosa <oarnosa@gmail.com>
2020-02-10 18:54:29 -0500

    Modify about page styles

--- src/components/showcase/showcase.component.jsx
+++ src/components/showcase/showcase.component.jsx
@@ -12,40 +12,40 @@ const Showcase = ({
   serverUrl,
   previewUrl
 }) => (
-  <div className='showcase'>
-    <div className='showcase-image'>
-      <img src={imageUrl} alt='' />
+  <div className="showcase">
+    <div className="showcase-image">
+      <img src={imageUrl} alt="" />
     </div>
-    <div className='showcase-info'>
-      <p className='featured'>Featured Project</p>
-      <h2 className='title'>{title}</h2>
-      <div className='description'>{description}</div>
-      <div className='skills'>
+    <div className="showcase-info">
+      <p className="featured">Featured Project</p>
+      <h2 className="title">{title}</h2>
+      <div className="description">{description}</div>
+      <div className="skills">
         {skills.map((skill, index) => (
-          <span key={index} className='skill'>
+          <span key={index} className="skill">
             {skill}
           </span>
         ))}
       </div>
-      <div className='links'>
+      <div className="links">
         <FontAwesomeIcon
-          className='link'
+          className="link"
           icon={['fab', 'github']}
-          size='lg'
+          size="lg"
           onClick={() => window.open(`${githubUrl}`)}
         />
         {serverUrl !== '' && (
           <FontAwesomeIcon
-            className='link'
+            className="link"
             icon={['fas', 'server']}
-            size='lg'
+            size="lg"
             onClick={() => window.open(`${serverUrl}`)}
           />
         )}
         <FontAwesomeIcon
-          className='link'
+          className="link"
           icon={['fas', 'external-link-alt']}
-          size='lg'
+          size="lg"
           onClick={() => window.open(`${previewUrl}`)}
         />
       </div>
--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -9,23 +9,21 @@ const AboutPage = () => (
       <h1 className='headline'>Get to Know Me</h1>
     </div>
     <section className='about'>
-      <div className='info'>
-        <p className='text'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut saepe
-          perspiciatis tenetur optio excepturi accusamus sapiente est minima
-          explicabo voluptas, dolorum dolore, exercitationem blanditiis
-          expedita, ex ad eum ducimus? Itaque.
-        </p>
-        <ul className='skillset'>
-          <li className='skill'>Javascript (ES6+)</li>
-          <li className='skill'>React</li>
-          <li className='skill'>Node.js</li>
-          <li className='skill'>Express</li>
-          <li className='skill'>HTML & (S)CSS</li>
-          <li className='skill'>PostgreSQL</li>
-          <li className='skill'>MongoDB</li>
-        </ul>
-      </div>
+      <p className='text'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut saepe
+        perspiciatis tenetur optio excepturi accusamus sapiente est minima
+        explicabo voluptas, dolorum dolore, exercitationem blanditiis expedita,
+        ex ad eum ducimus? Itaque.
+      </p>
+      <ul className='skillset'>
+        <li className='skill'>Javascript (ES6+)</li>
+        <li className='skill'>React</li>
+        <li className='skill'>Node.js</li>
+        <li className='skill'>Express</li>
+        <li className='skill'>HTML & (S)CSS</li>
+        <li className='skill'>PostgreSQL</li>
+        <li className='skill'>MongoDB</li>
+      </ul>
       <div className='image'>
         <img src='https://i.ibb.co/JzTLtGV/headshot-square.jpg' alt='' />
       </div>
