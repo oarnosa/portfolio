23ab26d795cd478418a6b6677f7088e546fae49d
Orlando Arnosa <oarnosa@gmail.com>
2020-02-12 17:15:50 -0500

    Include react-text-loop component for home page

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,42 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
+import TextLoop from "react-text-loop";
 
 import Button from "../../components/button/button.component";
 
 import "./home.styles.scss";
 
 const HomePage = () => {
-  // declare hooks
-  const [current, setCurrent] = useState(0);
   const history = useHistory();
 
-  // declare variables
-  const adjectives = [
-    "full-stack developer",
-    "front-end developer",
-    "back-end developer",
-    "software engineer",
-    "minimalist",
-    "gamer"
-  ];
-
-  // event handlers
   const handleOnClick = () => {
     history.push(`/about`);
   };
-
-  // lifecycle method to handle interval
-  useEffect(() => {
-    const interval = setInterval(() => {
-      let next = current + 1;
-      if (next > adjectives.length - 1) {
-        setCurrent(0);
-      } else {
-        setCurrent(next);
-      }
-    }, 2500);
-    return () => clearInterval(interval);
-  }, [current, adjectives]);
 
   return (
     <div className="page--home">
@@ -44,7 +19,20 @@ const HomePage = () => {
         <p className="hero__tag">Hey, my name is</p>
         <h1 className="hero__main">Orlando Arnosa.</h1>
         <h3 className="hero__sub">
-          I am a <span className="hero__sub__adj">{adjectives[current]}</span>.
+          I am a{" "}
+          <TextLoop
+            children={[
+              "full-stack developer",
+              "front-end developer",
+              "back-end developer",
+              "software engineer",
+              "minimalist",
+              "gamer"
+            ]}
+            springConfig={{ stiffness: 250, damping: 15 }}
+            mask
+          />
+          .
         </h3>
       </section>
       <Button onClick={handleOnClick}>Get to Know Me</Button>
