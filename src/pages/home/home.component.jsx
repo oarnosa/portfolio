6c50c66e39a952a8bcad133496d7349088a2debd
Orlando Arnosa <oarnosa@gmail.com>
2020-02-12 16:09:10 -0500

    Change class components to utilize hooks

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,56 +1,55 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 import Button from "../../components/button/button.component";
 
 import "./home.styles.scss";
 
-class HomePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      current: 0,
-      adj: [
-        "full-stack developer",
-        "front-end developer",
-        "back-end developer",
-        "software engineer",
-        "minimalist",
-        "gamer"
-      ]
-    };
-  }
+const HomePage = () => {
+  // declare hooks
+  const [current, setCurrent] = useState(0);
+  const history = useHistory();
 
-  handleOnClick = () => {
-    this.props.history.push(`/about`);
+  // declare variables
+  const adjectives = [
+    "full-stack developer",
+    "front-end developer",
+    "back-end developer",
+    "software engineer",
+    "minimalist",
+    "gamer"
+  ];
+
+  // event handlers
+  const handleOnClick = () => {
+    history.push(`/about`);
   };
 
-  cycleAdj = setInterval(() => {
-    let next = this.state.current + 1;
-    if (next > this.state.adj.length - 1) {
-      this.setState({ current: 0 });
-    } else {
-      this.setState({ current: next });
-    }
-  }, 3000);
+  // lifecycle method to handle interval
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let next = current + 1;
+      if (next > adjectives.length - 1) {
+        setCurrent(0);
+      } else {
+        setCurrent(next);
+      }
+    }, 2500);
+    return () => clearInterval(interval);
+  }, [current, adjectives]);
 
-  render() {
-    return (
-      <div className="page--home">
-        <section className="hero">
-          <p className="hero__tag">Hey, my name is</p>
-          <h1 className="hero__main">Orlando Arnosa.</h1>
-          <h3 className="hero__sub">
-            I am a{" "}
-            <span className="hero__sub__adj">
-              {this.state.adj[this.state.current]}
-            </span>
-            .
-          </h3>
-        </section>
-        <Button onClick={this.handleOnClick}>Get to Know Me</Button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="page--home">
+      <section className="hero">
+        <p className="hero__tag">Hey, my name is</p>
+        <h1 className="hero__main">Orlando Arnosa.</h1>
+        <h3 className="hero__sub">
+          I am a <span className="hero__sub__adj">{adjectives[current]}</span>.
+        </h3>
+      </section>
+      <Button onClick={handleOnClick}>Get to Know Me</Button>
+    </div>
+  );
+};
 
 export default HomePage;
--- src/pages/projects/projects.component.jsx
+++ src/pages/projects/projects.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Showcase from "../../components/showcase/showcase.component";
@@ -8,86 +8,77 @@ import PROJECT_DATA from "./projects.data";
 
 import "./projects.styles.scss";
 
-class ProjectsPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      projects: PROJECT_DATA,
-      current: 0
-    };
-  }
+const ProjectsPage = () => {
+  // declare hooks
+  const [current, setCurrent] = useState(0);
 
-  render() {
-    const { id, ...props } = this.state.projects[this.state.current];
+  // declare variables
+  const projects = PROJECT_DATA;
+  const { id, ...props } = projects[current];
 
-    const handlePrev = () => {
-      let prev = this.state.current - 1;
-      if (prev >= 0) this.setState({ current: prev });
-    };
+  // event handlers
+  const handleNext = () => {
+    let next = current + 1;
+    if (next <= projects.length - 1) setCurrent(next);
+  };
 
-    const handleNext = () => {
-      let next = this.state.current + 1;
-      if (next <= this.state.projects.length - 1)
-        this.setState({ current: next });
-    };
+  const handlePrev = () => {
+    let prev = current - 1;
+    if (prev >= 0) setCurrent(prev);
+  };
 
-    return (
-      <div className="page--projects">
-        <div className="heading">
-          <span className="heading__tag">02. Projects</span>
-          <h1 className="heading__main">Check Out My Work</h1>
-        </div>
-        <div className="carousel">
-          <div className="carousel__prev">
-            <FontAwesomeIcon
-              className="arrow"
-              icon={["fas", "chevron-left"]}
-              size="lg"
-              onClick={handlePrev}
-            />
-          </div>
-          <div className="carousel__project">
-            <Showcase key={id} {...props} />
-          </div>
-          <div className="carousel__next">
-            <FontAwesomeIcon
-              className="arrow"
-              icon={["fas", "chevron-right"]}
-              size="lg"
-              onClick={handleNext}
-            />
-          </div>
-        </div>
-        <div className="indicators">
-          {this.state.projects.map(project => (
-            <div
-              key={project.id}
-              className={`indicators__indicator ${
-                this.state.current === this.state.projects.indexOf(project)
-                  ? "current"
-                  : ""
-              }`}
-              onClick={() => {
-                this.setState({
-                  current: this.state.projects.indexOf(project)
-                });
-              }}
-            ></div>
-          ))}
-        </div>
-        <Button
-          style={{
-            alignSelf: "center"
-          }}
-          onClick={() =>
-            window.open("https://github.com/oarnosa?tab=repositories")
-          }
-        >
-          View More
-        </Button>
+  return (
+    <div className="page--projects">
+      <div className="heading">
+        <span className="heading__tag">02. Projects</span>
+        <h1 className="heading__main">Check Out My Work</h1>
       </div>
-    );
-  }
-}
+      <div className="carousel">
+        <div className="carousel__prev">
+          <FontAwesomeIcon
+            className="arrow"
+            icon={["fas", "chevron-left"]}
+            size="lg"
+            onClick={handlePrev}
+          />
+        </div>
+        <div className="carousel__project">
+          <Showcase key={id} {...props} />
+        </div>
+        <div className="carousel__next">
+          <FontAwesomeIcon
+            className="arrow"
+            icon={["fas", "chevron-right"]}
+            size="lg"
+            onClick={handleNext}
+          />
+        </div>
+      </div>
+      <div className="indicators">
+        {projects.map(project => (
+          <div
+            key={project.id}
+            className={`indicators__indicator ${
+              current === projects.indexOf(project) ? "current" : ""
+            }`}
+            onClick={() => {
+              setCurrent(projects.indexOf(project));
+            }}
+          ></div>
+        ))}
+      </div>
+      <Button
+        style={{
+          alignSelf: "center"
+        }}
+        onClick={() =>
+          window.open("https://github.com/oarnosa?tab=repositories")
+        }
+      >
+        View More
+      </Button>
+    </div>
+  );
+};
 
 export default ProjectsPage;
